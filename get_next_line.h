438b954fca3a1185b1b73891e07727a980202970
Vladyslav Tarasov <vtarasov@student.42warsaw.pl>
2026-07-09 14:18:22 +0200

    init

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtarasov <vtarasov@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/09 14:14:27 by vtarasov          #+#    #+#             */
+/*   Updated: 2026/07/09 14:15:34 by vtarasov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 1024
+
+char	*get_next_line(int fd);
+
+#endif
