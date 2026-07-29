0d227e79bbb3b378fedca6b98cd7cea3ac5fb946
Vladyslav Tarasov <vtarasov@student.42warsaw.pl>
2026-07-29 22:04:26 +0200

    header protection fix

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vtarasov <vtarasov@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/09 14:14:27 by vtarasov          #+#    #+#             */
-/*   Updated: 2026/07/29 21:31:57 by vtarasov         ###   ########.fr       */
+/*   Updated: 2026/07/29 22:04:16 by vtarasov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stddef.h>
 
 # ifndef BUFFER_SIZE
